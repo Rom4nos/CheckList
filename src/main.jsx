1ca055a673097212c87f88df0e1ecd33261a59e4
Mateus Romanos <112962881+Rom4nos@users.noreply.github.com>
2023-09-07 14:57:13 -0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ToDoList from './ToDoList'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ToDoList />
-  </React.StrictMode>,
-)
