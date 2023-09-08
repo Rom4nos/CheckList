0f6afb4b83699615393e05a5afa19ce97a2b8c38
Mateus Romanos <112962881+Rom4nos@users.noreply.github.com>
2023-09-08 15:08:54 -0400

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -46,7 +46,8 @@ function ToDoList(form){
         setIsLightMode(!isLightMode);
     };
 
-    const themeCssFile = isLightMode ? "/src/ToDoList.css" : "/src/ToDoListLight.css";
+    const themeCssFile = isLightMode ? "ToDoList.css" : "ToDoListLight.css";
+
 
     const iconSrc = isLightMode ? IconeDark : IconeLight;
 
